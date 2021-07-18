7b6e116456b31403092e74e20a869d919258eac3
ruiji <abcdabcd0517@icloud.com>
2021-07-18 12:32:11 +0900

    画像投稿ボタン追加、データベースに画像投稿のデータ確認OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_032135) do
+ActiveRecord::Schema.define(version: 2021_07_18_032734) do
 
   create_table "posts", charset: "utf8mb3", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
